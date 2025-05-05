39dab91d68e6bbfb85cbe1edfebe0b0a827697ab
Dave Mankoff <mankoff@google.com>
2025-05-05 09:43:24 -0700

    Revert "Revert "Split creating snapshot jars out of the compiler..."
    Revert submission 33373055-revert-32464406-b392906864-kotlin-incremental-RKSJYQSKFU
    
    Reason for revert: Restoring so I can fix it.
    
    Reverted changes: /q/submissionid:33373055-revert-32464406-b392906864-kotlin-incremental-RKSJYQSKFU
    
    Bug: 392906864
    Test: m && m
    Change-Id: I608e348c07006a1f204623973a6a2eddce4f9090

--- cmd/kotlinc_incremental/src/com/android/kotlin/compiler/cli/Argument.kt
+++ cmd/kotlinc_incremental/src/com/android/kotlin/compiler/cli/Argument.kt
@@ -32,7 +32,6 @@ fun <O : Options> parseArgs(
     var hasError = false
     var showHelp = args.isEmpty()
     val iter = args.iterator()
-
     while (iter.hasNext()) {
         val arg = iter.next()
         matched = false
